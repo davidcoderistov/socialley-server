b6d69d7c5368acb721fe97110a38841a4e56d765
davidcoderistov <davidcoderistov@gmail.com>
2022-12-05 16:18:45 +0100

    Parse getLatestMessage response from resolver

--- server/graphql/queries/messages.ts
+++ server/graphql/queries/messages.ts
@@ -1,21 +1,46 @@
-import Message from '../models/Message'
 import {
     ThunkObjMap,
     GraphQLFieldConfig,
     GraphQLList,
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLID,
+    GraphQLString,
 } from 'graphql'
 import { Context } from '../types'
 import messagesRepository from '../../repositories/messagesRepository'
 
 
+const LatestMessageUser = new GraphQLObjectType({
+    name: 'LatestMessageUser',
+    fields: () => ({
+        _id: { type: new GraphQLNonNull(GraphQLID) },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+    })
+})
+
+const LatestMessage = new GraphQLObjectType({
+    name: 'LatestMessage',
+    fields: () => ({
+        messageId: { type: new GraphQLNonNull(GraphQLID) },
+        fromUser: { type: LatestMessageUser },
+        toUser: { type: LatestMessageUser },
+        message: { type: GraphQLString },
+        photoURL: { type: GraphQLString },
+        createdAt: { type: new GraphQLNonNull(GraphQLString) },
+    })
+})
+
 const messagesQueries: ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
     getLatestMessages: {
-        type: new GraphQLList(Message),
+        type: new GraphQLList(LatestMessage),
         resolve: async (_, __, { userId }) => {
             const latest = await messagesRepository.getLatestMessages({ userId })
             return latest.map(message => ({
                 ...message,
-                _id: message.messageId,
+                fromUser: typeof message.fromUserId === 'string' ? { _id: message.fromUserId } : message.fromUserId,
+                toUser: typeof message.toUserId === 'string' ? { _id: message.toUserId } : message.toUserId,
             }))
         }
     }
