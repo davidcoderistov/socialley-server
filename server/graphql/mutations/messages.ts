44f66380cde68649572aa7b00c0bd99111276158
davidcoderistov <davidcoderistov@gmail.com>
2022-12-05 14:37:12 +0100

    Refactor createMessage mutation

--- server/graphql/mutations/messages.ts
+++ server/graphql/mutations/messages.ts
@@ -12,23 +12,25 @@ import { pubsub } from '../../config/server'
 import { MESSAGES_SUBSCRIPTIONS } from '../subscriptions/messages'
 
 
-const CreateInput = new GraphQLInputObjectType({
+const CreateMessageInput = new GraphQLInputObjectType({
     name: 'CreateMessageInput',
     fields: () => ({
-        to: { type: new GraphQLNonNull(GraphQLString) },
-        message: { type: new GraphQLNonNull(GraphQLString) },
+        toUserId: { type: new GraphQLNonNull(GraphQLString) },
+        message: { type: GraphQLString },
+        photoURL: { type: GraphQLString },
     })
 })
 
 const messagesMutations: ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
     createMessage: {
         type: Message,
-        args: { message: { type: CreateInput }},
+        args: { message: { type: CreateMessageInput }},
         resolve: async (_, { message }, { userId }) => {
             const createdMessage = await messagesRepository.createMessage({
-                from: userId,
-                to: message.to,
+                fromUserId: userId,
+                toUserId: message.toUserId,
                 message: message.message,
+                photoURL: message.photoURL,
             })
             pubsub.publish(MESSAGES_SUBSCRIPTIONS.MESSAGE_CREATED, createdMessage)
             return createdMessage
