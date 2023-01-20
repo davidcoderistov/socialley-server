d6de251fd3486c6dd6faf3152561d1773d132977
davidcoderistov <davidcoderistov@gmail.com>
2023-01-20 14:37:20 +0100

    Create CreateMessage graphql model

--- server/graphql/mutations/messages.ts
+++ server/graphql/mutations/messages.ts
@@ -3,14 +3,29 @@ import {
     GraphQLFieldConfig,
     GraphQLInputObjectType,
     GraphQLNonNull,
+    GraphQLObjectType,
+    GraphQLID,
     GraphQLString,
 } from 'graphql'
-import Message from '../models/Message'
+import { DateScalar } from '../scalars'
 import messagesRepository from '../../repositories/messagesRepository'
 import { Context } from '../types'
 import { pubsub } from '../../config/server'
 import { MESSAGES_SUBSCRIPTIONS } from '../subscriptions/messages'
+import PublicUser from "../models/PublicUser";
 
+
+const CreatedMessage = new GraphQLObjectType({
+    name: 'CreatedMessage',
+    fields: () => ({
+        _id: { type: new GraphQLNonNull(GraphQLID) },
+        fromUser: { type: new GraphQLNonNull(PublicUser) },
+        toUser: { type: new GraphQLNonNull(PublicUser) },
+        message: { type: GraphQLString },
+        photoURL: { type: GraphQLString },
+        createdAt: { type: new GraphQLNonNull(DateScalar) },
+    })
+})
 
 const CreateMessageInput = new GraphQLInputObjectType({
     name: 'CreateMessageInput',
@@ -23,7 +38,7 @@ const CreateMessageInput = new GraphQLInputObjectType({
 
 const messagesMutations: ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
     createMessage: {
-        type: Message,
+        type: CreatedMessage,
         args: { message: { type: CreateMessageInput }},
         resolve: async (_, { message }, { userId }) => {
             const createdMessage = await messagesRepository.createMessage({
