9e8bcc44f0b41b1f60aa06bb2677d73849b92c80
davidcoderistov <davidcoderistov@gmail.com>
2023-01-25 16:51:41 +0100

    Add getUsersBySearchQuery graphql query

--- server/graphql/queries/user.ts
+++ server/graphql/queries/user.ts
@@ -1,19 +1,27 @@
-import User from '../models/User'
+import PublicUser from '../models/PublicUser'
 import {
     ThunkObjMap,
     GraphQLFieldConfig,
     GraphQLList,
+    GraphQLNonNull,
+    GraphQLString,
+    GraphQLInt,
 } from 'graphql'
+import { Context } from '../types'
+import userRepository from '../../repositories/userRepository'
 
 
-const userQueries: ThunkObjMap<GraphQLFieldConfig<any, any, any>> = {
-    users: {
-        type: new GraphQLList(User),
-        resolve () {
-            return []
+const userQueries: ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
+    getUsersBySearchQuery: {
+        type: new GraphQLList(PublicUser),
+        args: {
+            searchQuery: { type: new GraphQLNonNull(GraphQLString) },
+            limit: { type: new GraphQLNonNull(GraphQLInt) },
+        },
+        resolve (_, { searchQuery, limit }) {
+            return userRepository.findUsersBySearchQuery({ searchQuery, limit })
         }
     }
 }
 
 export default userQueries
-
