20310e7a73f88b5f34e4b5c51ed77d2bd5de2c5d
davidcoderistov <davidcoderistov@gmail.com>
2023-02-25 17:42:37 +0100

    Refactor auth mutations

--- server/graphql/mutations/auth.ts
+++ server/graphql/mutations/auth.ts
@@ -6,12 +6,13 @@ import {
     GraphQLString,
 } from 'graphql'
 import userRepository from '../../repositories/userRepository'
-import User from '../models/User'
+import User from '../models/user/User'
+import AuthUser from '../models/user/AuthUser'
 import { Context } from '../types'
 
 
-const SignUpInput = new GraphQLInputObjectType({
-    name: 'SignUpInput',
+const SignUpOptions = new GraphQLInputObjectType({
+    name: 'SignUpOptions',
     fields: () => ({
         username: { type: new GraphQLNonNull(GraphQLString) },
         firstName: { type: new GraphQLNonNull(GraphQLString) },
@@ -21,8 +22,8 @@ const SignUpInput = new GraphQLInputObjectType({
     })
 })
 
-const LoginInput = new GraphQLInputObjectType({
-    name: 'LoginInput',
+const LoginOptions = new GraphQLInputObjectType({
+    name: 'LoginOptions',
     fields: () => ({
         username: { type: new GraphQLNonNull(GraphQLString) },
         password: { type: new GraphQLNonNull(GraphQLString) },
@@ -32,27 +33,33 @@ const LoginInput = new GraphQLInputObjectType({
 const authMutations:  ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
     signUp: {
         type: User,
-        args: { user: { type: SignUpInput }},
+        args: { user: { type: SignUpOptions }},
         resolve: (_, { user }) => {
             return userRepository.signUp(user)
         }
     },
     login: {
-        type: User,
-        args: { user: { type: LoginInput }},
+        type: AuthUser,
+        args: { user: { type: LoginOptions }},
         resolve: async (_, { user }, { setRefreshTokenCookie }) => {
             const loggedInUser = await userRepository.login(user)
             setRefreshTokenCookie(loggedInUser.refreshToken)
-            return loggedInUser
+            return {
+                user: loggedInUser,
+                accessToken: loggedInUser.accessToken
+            }
         }
     },
     refresh: {
-        type: User,
+        type: AuthUser,
         resolve: async (_, __, { setRefreshTokenCookie, getRefreshTokenCookie }) => {
             const refreshToken = getRefreshTokenCookie()
             const user = await userRepository.refresh({ refreshToken })
             setRefreshTokenCookie(user.refreshToken)
-            return user
+            return {
+                user,
+                accessToken: user.accessToken
+            }
         }
     },
     logout: {
