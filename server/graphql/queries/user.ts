be278783525f392bff10dc9c32c1ce3073cfb5d2
davidcoderistov <davidcoderistov@gmail.com>
2023-02-26 10:26:02 +0100

    Refactor getSuggestedUsers graphql query

--- server/graphql/queries/user.ts
+++ server/graphql/queries/user.ts
@@ -1,4 +1,3 @@
-import PublicUser from '../models/PublicUser'
 import {
     ThunkObjMap,
     GraphQLFieldConfig,
@@ -7,9 +6,9 @@ import {
     GraphQLString,
     GraphQLInt,
     GraphQLObjectType,
-    GraphQLID,
 } from 'graphql'
 import User from '../models/user/User'
+import FollowableUser from '../models/user/FollowableUser'
 import { Context } from '../types'
 import userRepository from '../../repositories/userRepository'
 
@@ -17,15 +16,12 @@ import userRepository from '../../repositories/userRepository'
 const SuggestedUser = new GraphQLObjectType({
     name: 'SuggestedUser',
     fields: () => ({
-        _id: { type: new GraphQLNonNull(GraphQLID) },
-        username: { type: new GraphQLNonNull(GraphQLString) },
-        firstName: { type: new GraphQLNonNull(GraphQLString) },
-        lastName: { type: new GraphQLNonNull(GraphQLString) },
-        avatarURL: { type: GraphQLString },
-        latestFollower: { type: PublicUser },
+        followableUser: { type: new GraphQLNonNull(FollowableUser) },
+        latestFollower: { type: User },
         followedCount: { type: new GraphQLNonNull(GraphQLInt) }
     })
 })
+
 const userQueries: ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
     getUsersBySearchQuery: {
         type: new GraphQLNonNull(new GraphQLList(User)),
@@ -38,8 +34,18 @@ const userQueries: ThunkObjMap<GraphQLFieldConfig<any, Context>> = {
         }
     },
     getSuggestedUsers: {
-        type: new GraphQLList(SuggestedUser),
-        resolve: (_, __, { userId }) => userRepository.getSuggestedUsers({ userId })
+        type: new GraphQLNonNull(new GraphQLList(SuggestedUser)),
+        resolve: async (_, __, { userId }) => {
+            const suggestedUsers = await userRepository.getSuggestedUsers({ userId })
+            return suggestedUsers.map(suggestedUser => ({
+                followableUser: {
+                    user: { ...suggestedUser },
+                    following: false,
+                },
+                latestFollower: suggestedUser.latestFollower,
+                followedCount: suggestedUser.followedCount,
+            }))
+        }
     }
 }
 
