08102c3e2b7cd38ffcbaae542f3e4fd571f0f334
davidcoderistov <davidcoderistov@gmail.com>
2023-02-26 13:38:08 +0100

    Refactor Comment graphql model

--- server/graphql/models/Comment.ts
+++ server/graphql/models/Comment.ts
@@ -1,19 +1,23 @@
 import {
     GraphQLObjectType,
-    GraphQLID,
     GraphQLString,
     GraphQLNonNull,
+    GraphQLBoolean,
+    GraphQLInt,
 } from 'graphql'
 import { DateScalar } from '../scalars'
+import User from './user/User'
 
 
 const Comment = new GraphQLObjectType({
     name: 'Comment',
     fields: () => ({
-        _id: { type: new GraphQLNonNull(GraphQLID) },
+        _id: { type: new GraphQLNonNull(GraphQLString) },
         text: { type: new GraphQLNonNull(GraphQLString) },
         postId: { type: new GraphQLNonNull(GraphQLString) },
-        userId: { type: new GraphQLNonNull(GraphQLString) },
+        user: { type: new GraphQLNonNull(User) },
+        liked: { type: new GraphQLNonNull(GraphQLBoolean) },
+        likesCount: { type: new GraphQLNonNull(GraphQLInt) },
         createdAt: { type: new GraphQLNonNull(DateScalar) },
     })
 })
