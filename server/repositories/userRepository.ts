8b824315fe4803014c3402aef82b2d8183e6e3a2
davidcoderistov <davidcoderistov@gmail.com>
2023-03-09 14:40:38 +0100

    Do not include logged in user as mutual follower

--- server/repositories/userRepository.ts
+++ server/repositories/userRepository.ts
@@ -693,7 +693,6 @@ async function getUserDetails ({ userId, loggedInUserId }: GetUserDetailsOptions
 
             const userFollows = await Follow.find({ followingUserId: loggedInUserId }).select('followedUserId')
             const followedUsersIds = userFollows.map(follow => follow.followedUserId)
-            followedUsersIds.push(loggedInUserId)
 
             const mutualFollowers = await Follow.find({
                 $and: [
