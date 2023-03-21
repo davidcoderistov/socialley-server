72e027db9109195d3d418c8950b793b47738299f
davidcoderistov <davidcoderistov@gmail.com>
2023-03-21 16:58:48 +0100

    Implement cache getFollowersForUser query result

---
+++ server/loaders/followersForUserLoader.ts
@@ -0,0 +1,29 @@
+import DataLoader from 'dataloader'
+import userRepository from '../repositories/userRepository'
+
+
+interface LoaderKey {
+    loggedInUserId: string
+    userId: string
+}
+
+const generateKey = (loaderKey: LoaderKey) => `${loaderKey.loggedInUserId}-${loaderKey.userId}`
+
+const followersForUserLoader = new DataLoader(async (keys: LoaderKey[]) => {
+    const followersForUserByUser = await Promise.all(
+        keys.map(async ({ loggedInUserId, userId }) => {
+            const followersForUser = await userRepository.getFollowersForUser({ loggedInUserId, userId })
+            return { loggedInUserId, userId, followersForUser }
+        })
+    )
+
+    const followersForUserByUserObject = followersForUserByUser
+        .reduce((result, { loggedInUserId, userId, followersForUser }) => ({
+            ...result,
+            [generateKey({ loggedInUserId, userId })]: followersForUser
+        }), {})
+
+    return keys.map(key => followersForUserByUserObject[generateKey(key)])
+})
+
+export default followersForUserLoader
