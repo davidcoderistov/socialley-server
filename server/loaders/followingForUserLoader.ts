189021492b001f82b464910dd00ebcf942fd778d
davidcoderistov <davidcoderistov@gmail.com>
2023-03-21 16:48:26 +0100

    Implement cache getFollowingForUser query result

---
+++ server/loaders/followingForUserLoader.ts
@@ -0,0 +1,29 @@
+import DataLoader from 'dataloader'
+import userRepository from '../repositories/userRepository'
+
+
+interface LoaderKey {
+    loggedInUserId: string
+    userId: string
+}
+
+const generateKey = (loaderKey: LoaderKey) => `${loaderKey.loggedInUserId}-${loaderKey.userId}`
+
+const followingForUserLoader = new DataLoader(async (keys: LoaderKey[]) => {
+    const followingForUserByUser = await Promise.all(
+        keys.map(async ({ loggedInUserId, userId }) => {
+            const followingForUser = await userRepository.getFollowingForUser({ loggedInUserId, userId })
+            return { loggedInUserId, userId, followingForUser }
+        })
+    )
+
+    const followingForUserByUserObject = followingForUserByUser
+        .reduce((result, { loggedInUserId, userId, followingForUser }) => ({
+            ...result,
+            [generateKey({ loggedInUserId, userId })]: followingForUser
+        }), {})
+
+    return keys.map(key => followingForUserByUserObject[generateKey(key)])
+})
+
+export default followingForUserLoader
