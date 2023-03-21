19cd5d9dfbc656653f9c88c38350560cc7718b6f
davidcoderistov <davidcoderistov@gmail.com>
2023-03-21 16:36:02 +0100

    Implement cache getUsersWhoLikedComment query result

---
+++ server/loaders/usersWhoLikedCommentLoader.ts
@@ -0,0 +1,29 @@
+import DataLoader from 'dataloader'
+import postsRepository from '../repositories/postsRepository'
+
+
+interface LoaderKey {
+    userId: string
+    commentId: string
+}
+
+const generateKey = (loaderKey: LoaderKey) => `${loaderKey.userId}-${loaderKey.commentId}`
+
+const usersWhoLikedCommentLoader = new DataLoader(async (keys: LoaderKey[]) => {
+    const usersWhoLikedCommentByUser = await Promise.all(
+        keys.map(async ({ userId, commentId }) => {
+            const usersWhoLikedComment = await postsRepository.getUsersWhoLikedComment({ userId, commentId })
+            return { userId, commentId, usersWhoLikedComment }
+        })
+    )
+
+    const usersWhoLikedCommentByUserObject = usersWhoLikedCommentByUser
+        .reduce((result, { userId, commentId, usersWhoLikedComment }) => ({
+            ...result,
+            [generateKey({ userId, commentId })]: usersWhoLikedComment
+        }), {})
+
+    return keys.map(key => usersWhoLikedCommentByUserObject[generateKey(key)])
+})
+
+export default usersWhoLikedCommentLoader
