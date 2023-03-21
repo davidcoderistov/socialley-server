d13d2128e080ed35d29155dfcf21f9e527f221f2
davidcoderistov <davidcoderistov@gmail.com>
2023-03-21 16:23:44 +0100

    Implement cache getUsersWhoLikedPost query result

---
+++ server/loaders/usersWhoLikedPostLoader.ts
@@ -0,0 +1,29 @@
+import DataLoader from 'dataloader'
+import postsRepository from '../repositories/postsRepository'
+
+
+interface LoaderKey {
+    userId: string
+    postId: string
+}
+
+const generateKey = (loaderKey: LoaderKey) => `${loaderKey.userId}-${loaderKey.postId}`
+
+const usersWhoLikedPostLoader = new DataLoader(async (keys: LoaderKey[]) => {
+    const usersWhoLikedPostByUser = await Promise.all(
+        keys.map(async ({ userId, postId }) => {
+            const usersWhoLikedPost = await postsRepository.getUsersWhoLikedPost({ userId, postId })
+            return { userId, postId, usersWhoLikedPost }
+        })
+    )
+
+    const usersWhoLikedPostByUserObject = usersWhoLikedPostByUser
+        .reduce((result, { userId, postId, usersWhoLikedPost }) => ({
+            ...result,
+            [generateKey({ userId, postId })]: usersWhoLikedPost
+        }), {})
+
+    return keys.map(key => usersWhoLikedPostByUserObject[generateKey(key)])
+})
+
+export default usersWhoLikedPostLoader
