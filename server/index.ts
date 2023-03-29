5e515bb746c4b22c126b95f2fa635beb4be08770
davidcoderistov <davidcoderistov@gmail.com>
2023-03-29 17:01:30 +0200

    Revert "Connect before listen"
    This reverts commit 8569013166b939eba0dabbd4d1790e2e88ec3b7b.

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ import setupServer from './config/server'
 dotenv.config()
 
 // Connect to database
-connectDB().then(() => {
-    // Set up server
-    setupServer().catch(console.log)
-}).catch(console.log)
+connectDB()
+
+// Set up server
+setupServer()
