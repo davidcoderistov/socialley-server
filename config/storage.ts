0dfb7c6f2ffb1b240f462ef54f814d62e263a60c
davidcoderistov <davidcoderistov@gmail.com>
2023-03-31 17:36:25 +0200

    Recursive mkdir

--- config/storage.ts
+++ config/storage.ts
@@ -5,22 +5,22 @@ const initStorage = () => {
     const storageDir = process.env.NODE_ENV !== 'production' ? 'storage' : 'dist/storage'
 
     if (!fs.existsSync(storageDir)) {
-        fs.mkdirSync(storageDir)
+        fs.mkdirSync(storageDir, { recursive: true })
     }
 
     const postsDir = `${storageDir}/posts`
     if (!fs.existsSync(postsDir)) {
-        fs.mkdirSync(postsDir)
+        fs.mkdirSync(postsDir, { recursive: true })
     }
 
     const messagesDir = `${storageDir}/messages`
     if (!fs.existsSync(messagesDir)) {
-        fs.mkdirSync(messagesDir)
+        fs.mkdirSync(messagesDir, { recursive: true })
     }
 
     const avatarsDir = `${storageDir}/avatars`
     if (!fs.existsSync(avatarsDir)) {
-        fs.mkdirSync(avatarsDir)
+        fs.mkdirSync(avatarsDir, { recursive: true })
     }
 }
 
