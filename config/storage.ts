6d7b80f0a2d0cd8b1cfbc4c5edfb3091e927e1d8
davidcoderistov <davidcoderistov@gmail.com>
2023-04-01 15:19:38 +0200

    Revert "Recursive mkdir"
    This reverts commit 0dfb7c6f2ffb1b240f462ef54f814d62e263a60c.

--- config/storage.ts
+++ config/storage.ts
@@ -5,22 +5,22 @@ const initStorage = () => {
     const storageDir = process.env.NODE_ENV !== 'production' ? 'storage' : 'dist/storage'
 
     if (!fs.existsSync(storageDir)) {
-        fs.mkdirSync(storageDir, { recursive: true })
+        fs.mkdirSync(storageDir)
     }
 
     const postsDir = `${storageDir}/posts`
     if (!fs.existsSync(postsDir)) {
-        fs.mkdirSync(postsDir, { recursive: true })
+        fs.mkdirSync(postsDir)
     }
 
     const messagesDir = `${storageDir}/messages`
     if (!fs.existsSync(messagesDir)) {
-        fs.mkdirSync(messagesDir, { recursive: true })
+        fs.mkdirSync(messagesDir)
     }
 
     const avatarsDir = `${storageDir}/avatars`
     if (!fs.existsSync(avatarsDir)) {
-        fs.mkdirSync(avatarsDir, { recursive: true })
+        fs.mkdirSync(avatarsDir)
     }
 }
 
